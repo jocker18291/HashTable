e7dd298e80532597f0afc67a6fd3306f5640cbc8
jocker18291 <dawidans714@gmail.com>
2025-05-27 11:29:15 +0200

    Add public methods to class

--- include/HashTableAVL.hpp
+++ include/HashTableAVL.hpp
@@ -1,4 +1,14 @@
 #ifndef HASH_TABLE_AVL_HPP
 #define HASH_TABLE_AVL_HPP
 
+class HashAVL {
+private:
+
+public:
+    HashAVL();
+    ~HashAVL();
+    void insert(int key, int value);
+    void remove(int key);
+};
+
 #endif
