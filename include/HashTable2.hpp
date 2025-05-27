423d3c9564c4312c6eb9895dd60f8ae9b46ed114
jocker18291 <dawidans714@gmail.com>
2025-05-27 21:01:04 +0200

    Test

--- include/HashTable2.hpp
+++ include/HashTable2.hpp
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-#ifndef HASH_TABLE2_HPP
-#define HASH_TABLE2_HPP
-
-class HashTable2
-{
-private:
-	struct Node
-	{
-		int key;
-		int value;
-		Node* next;
-	};
-
-	Node** table;
-	int size;
-
-public:
-	HashTable2(int table_size);
-	~HashTable2();
-	void insert(int key, int value);
-	void remove(int key);
-	int hash_function(int key);
-};
-=======
 #ifndef HASH_TABLE2_HPP
 #define HASH_TABLE2_HPP
 
@@ -47,5 +22,5 @@ public:
 	int hash_function(int key);
 	void print();
 };
->>>>>>> f089aa3a0bcb34475ede9c23f2e80dccd7943f4e
+
 #endif
