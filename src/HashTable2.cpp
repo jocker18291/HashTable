f0522619e5ccddd57863658bdc4f37c691922609
MesnerH <hubertimssar04@gmail.com>
2025-05-27 03:03:34 +0200

    added insert and remove function

--- src/HashTable2.cpp
+++ src/HashTable2.cpp
@@ -30,3 +30,51 @@ int HashTable2::hash_function(int key)
 {
 	return key % size;
 }
+
+void HashTable2::insert(int key, int value)
+{
+	int index = hash_function(key);
+	Node* current = table[index];
+
+	while (current != nullptr)
+	{
+		if (current->key == key)
+		{
+			current->value = value;
+			return;
+		}
+		current = current->next;
+	}
+
+	Node* new_node = new Node;
+	new_node->key = key;
+	new_node->value = value;
+	new_node->next = table[index];
+	table[index] = new_node;
+}
+
+void HashTable2::remove(int key)
+{
+	int index = hash_function(key);
+	Node* current = table[index];
+	Node* previous_node = nullptr;
+
+	while (current != nullptr)
+	{
+		if (current->key == key)
+		{
+			if (previous_node == nullptr)
+			{
+				table[index] = current->next;
+			}
+			else
+			{
+				previous_node->next = current->next;
+			}
+			delete current;
+			return;
+		}
+		previous_node = current;
+		current = current->next;
+	}
+}
