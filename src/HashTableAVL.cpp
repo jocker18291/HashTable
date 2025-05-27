da9c9c4ec43fe6bf734c766edb8815ab8258d2ab
jocker18291 <dawidans714@gmail.com>
2025-05-27 21:07:25 +0200

    Constructor initialized

--- src/HashTableAVL.cpp
+++ src/HashTableAVL.cpp
@@ -1,1 +1,11 @@
 #include "../include/HashTableAVL.hpp"
+#include <iostream>
+#include <cstdlib>
+
+HashAVL::HashAVL() {
+    size = 100003;
+    table = new AVLNode*[size];
+    for(int i = 0; i < size; i++) {
+        table[i] = nullptr;
+    }
+}
